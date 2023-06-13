c139a6a84ceb472bbe294bc426d448e680240fc2
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2023-06-13 10:42:28 +0530

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    screens: {
+      'xs': '450px',
+      ...defaultTheme.screens,
+    }
+  },
+  plugins: [],
+}
