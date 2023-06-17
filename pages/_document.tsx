320561e4c85aa831d9322d05661e95c13222c1bc
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2023-06-17 11:36:36 +0530

    Added Inter Font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-gray-900 text-white min-h-screen px-6">
+      <body
+        className={`${inter.className}bg-gray-900 text-white min-h-screen px-6`}
+      >
         <Main />
         <NextScript />
       </body>
